ec887d08fafe044cfe042081c0f019b4afac8c9c
Saad Ahmed <saad.sengr@gmail.com>
2021-10-04 13:33:08 +0500

    KLine Socket added.

---
+++ src/KLine/KLine.module.ts
@@ -0,0 +1,8 @@
+   
+import { Module } from '@nestjs/common';
+import { KLineGateway } from './KLine.gateway';
+
+@Module({
+  providers: [KLineGateway],
+})
+export class KLineSocketModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { KLineSocketModule } from './KLine/KLine.module';
 @Module({
-  imports: [],
+  imports: [KLineSocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
