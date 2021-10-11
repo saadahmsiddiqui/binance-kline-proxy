fd7c0439cc43057bdfec54cfddb31396db869695
Saad Ahmed <saad.sengr@gmail.com>
2021-10-11 15:45:44 +0500

    ProcFile

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useWebSocketAdapter(new WsAdapter(app));
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
