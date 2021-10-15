d22ffbd312d37f6e9492b17ae145818bd2db2afc
Saad Ahmed <saad.sengr@gmail.com>
2021-10-15 10:36:22 +0500

    Returns added.

--- src/KLine/KLine.service.ts
+++ src/KLine/KLine.service.ts
@@ -17,16 +17,16 @@ export class KLineService {
                 .get(`https://api3.binance.com/api/v3/klines?${query}`)
                 .then((axiosRes) => {
                     if (axiosRes.status === 200) {
-                        res(axiosRes.data);
+                        return res(axiosRes.data);
                     } else {
-                        rej(new HttpException(axiosRes.data, axiosRes.status));
+                        return rej(new HttpException(axiosRes.data, axiosRes.status));
                     }
                 })
                 .catch((err) => {
                     if (err.response) {
                         // The request was made and the server responded with a status code
                         // that falls out of the range of 2xx
-                        rej(
+                        return rej(
                             new HttpException(
                                 err.response.data,
                                 err.response.status,
@@ -37,7 +37,7 @@ export class KLineService {
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                         // http.ClientRequest in node.js
                         console.log(err.request);
-                        rej(
+                        return rej(
                             new HttpException(
                                 err.response.data,
                                 err.response.status,
@@ -45,7 +45,7 @@ export class KLineService {
                         );
                     } else {
                         // Something happened in setting up the request that triggered an Error
-                        rej(new HttpException(err.message, 500));
+                        return rej(new HttpException(err.message, 500));
                     }
                 });
         });
